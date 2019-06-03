49f6903ce609535cfab5f886b2f3ee45bc8626a0
Meghan Miller <mille265@gmail.com>
2019-06-03 10:03:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
-
-
-end
+  <<<<<<< HEAD
+  
+    # Define your GET '/' route below and respond with "Hello, World!"
+    get '/' do
+      "Hello, World!"
+    end
+  
+  end
+  =======
+    get '/' do
+      "Hello, World!"
+    end
+  end
+  >>>>>>> solution with contributing file and .learn tags
